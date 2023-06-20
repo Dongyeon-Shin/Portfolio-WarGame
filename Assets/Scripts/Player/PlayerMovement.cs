d126d8487d3b9ba804c25a62a67f4314bf9c5510
Dongyeon Shin <d.shin0920@gmail.com>
2023-06-20 21:33:32 +0900

    feat: add Player move under command
    코루틴을 사용해 매개변수로 목표지점의 위치를 넣으면 해당 지점으로 캐릭터가 자동으로 이동하고 그동안 이동 입력이나 카메라에 의한 이동방향 영향을 무시하는 기능

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,8 +17,13 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 moveDirection;
     private float ySpeed = 0;
     private Animator animator;
-    private bool walk;
+    private bool walk = true;
     private float moveSpeed;
+    private bool outofControl = false;
+    private bool cameraDirectionBinding = true;
+    // TODO: tempPoint 삭제
+    [SerializeField]
+    private Transform tempPoint;
 
     private void Awake()
     {
@@ -29,7 +36,10 @@ public class PlayerMovement : MonoBehaviour
         Move();
         Fall();
     }
-
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Confined;    
+    }
     private void Move()
     {
         if (moveDirection.magnitude == 0)
@@ -38,8 +48,18 @@ public class PlayerMovement : MonoBehaviour
             animator.SetFloat("MoveSpeed", moveSpeed);
             return;
         }
-        Vector3 forwardVector = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
-        Vector3 rightVector = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
+        Vector3 forwardVector;
+        Vector3 rightVector;
+        if (cameraDirectionBinding)
+        {
+            forwardVector = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized;
+            rightVector = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized;
+        }
+        else
+        {
+            forwardVector = Vector3.forward;
+            rightVector = Vector3.right;
+        }
         if (walk)
         {
             moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, 0.1f);
@@ -52,15 +72,58 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(rightVector * moveDirection.x * moveSpeed * Time.deltaTime);
         animator.SetFloat("MoveSpeed", moveSpeed);
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(forwardVector * moveDirection.z + rightVector * moveDirection.x), 0.1f);
-
     }
-
     private void OnMove(InputValue value)
     {
+        if (outofControl)
+        {
+            return;
+        }
         moveDirection.x = value.Get<Vector2>().x;
         moveDirection.z = value.Get<Vector2>().y;
+        Debug.Log(moveDirection.z);
     }
-
+    // TODO: 인터페이스 구현하는것도 생각해볼것
+    public IEnumerator HeadtoPositon(Vector3 positon)
+    {
+        outofControl = true;
+        cameraDirectionBinding = false;
+        Vector3 direction = (positon - transform.position).normalized;
+        if (Vector3.Dot(transform.forward, direction) < 0)
+        {
+            outofControl = false;
+            cameraDirectionBinding = true;
+            // TODO: 해당 방향으로 부드럽게 회전하는 코드 구현
+            yield break;
+        }
+        moveDirection.z = 1f;
+        float distance = (positon - transform.position).sqrMagnitude;
+        float learpRate = 0.005f;
+        while (distance > 0.1f)
+        {
+            yield return null;
+            distance = (positon - transform.position).sqrMagnitude;
+            direction = positon - transform.position;
+            // TODO: 진행방향을 바꾸는 모션을 조금 더 자연스럽게 할것
+            if (direction.x > 0)
+            {
+                moveDirection.x = Mathf.Lerp(moveDirection.x, 0.71f, 1f * learpRate);
+            }
+            else if (direction.x < 0)
+            {
+                moveDirection.x = Mathf.Lerp(moveDirection.x, -0.71f, 1f * learpRate);
+            }
+            else
+            {
+                moveDirection.x = 0;
+            }
+            learpRate += learpRate * 0.01f;
+        }
+        moveDirection.x = 0f;
+        moveDirection.z = 0f;
+        outofControl = false;
+        cameraDirectionBinding = true;
+    }
     private void Fall()
     {
         ySpeed += Physics.gravity.y * Time.deltaTime;
@@ -70,17 +133,18 @@ public class PlayerMovement : MonoBehaviour
         }
         controller.Move(Vector3.up * ySpeed * Time.deltaTime);
     }
-
     private void Jump()
     {
         ySpeed = jumpPower;
     }
-
     private void OnJump(InputValue value)
     {
+        if (outofControl)
+        {
+            return;
+        }
         Jump();
     }
-
     private void OnRun(InputValue value)
     {
         walk = !value.isPressed;
