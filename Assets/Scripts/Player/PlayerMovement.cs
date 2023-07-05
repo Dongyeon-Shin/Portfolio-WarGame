41d53d779d87c60a835dfe781b76ceece7669e21
Dongyeon Shin <d.shin0920@gmail.com>
2023-07-05 13:31:53 +0900

    fix: solve slope check issue and add wade motion
    경사의 방향이 제대로 감지되지 않던 오류를 수정하고 물이나 진창및 오르막길 내리막길 걷는 모션 변경

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float runSpeed;
     private CharacterController controller;
+    private CapsuleCollider hitbox;
     private Vector3 moveDirection;
     private float ySpeed = 0;
     private Animator animator;
@@ -24,10 +25,15 @@ public class PlayerMovement : MonoBehaviour
     public bool RideOnHorseback { get { return rideOnHorseback; } }
     private Horse ridingHorse;
     private PlayerController playerController;
+    private float gradient;
     private float currentSlope;
-    private float slopeSpeed;
+    private float onSteepTime;
     private bool floating;
     private float floatingTime;
+    private bool decelerate;
+    private LayerMask groundLayerMask;
+    private LayerMask slowAreaLayerMask;
+    private LayerMask slipperyAreaLayerMask;
 
     // 테스트 코드
     [SerializeField]
@@ -37,8 +43,12 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        hitbox = GetComponent<CapsuleCollider>();
         animator = GetComponent<Animator>();
         playerController = GetComponent<PlayerController>();
+        slowAreaLayerMask = LayerMask.GetMask("Mire") | LayerMask.GetMask("Water");
+        slipperyAreaLayerMask = LayerMask.GetMask("Slippery");
+        groundLayerMask = LayerMask.GetMask("Environment") | slowAreaLayerMask | slipperyAreaLayerMask;
     }
     private void OnDisable()
     {
@@ -68,10 +78,8 @@ public class PlayerMovement : MonoBehaviour
             {
                 moveSpeed = Mathf.Lerp(moveSpeed, runSpeed, 0.05f);
             }
-            if (currentSlope < 0.9f)
-            {
-                moveSpeed = Mathf.Lerp(moveSpeed, slopeSpeed * moveSpeed, 0.05f);
-            }
+            SlopeCheck();
+            DecelerateCheck();
             controller.Move(forwardVector * moveDirection.z * moveSpeed * Time.deltaTime);
             controller.Move(rightVector * moveDirection.x * moveSpeed * Time.deltaTime);
             animator.SetFloat("MoveSpeed", moveSpeed);
@@ -86,6 +94,62 @@ public class PlayerMovement : MonoBehaviour
         }
         moveDirection.x = value.Get<Vector2>().x;
         moveDirection.z = value.Get<Vector2>().y;
+    }
+    private void DecelerateCheck()
+    {
+        if (decelerate)
+        {
+            animator.SetBool("Decelerate", true);
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, 0.1f);
+            controller.height = 1.5f;
+            controller.center = new Vector3 (controller.center.x, 1.25f, controller.center.z);
+            hitbox.height = 1.5f;
+            hitbox.center = new Vector3(hitbox.center.x, 1.25f, hitbox.center.z);
+        }
+        else
+        {
+            animator.SetBool("Decelerate", false);
+            controller.height = 1.8f;
+            controller.center = new Vector3(controller.center.x, Mathf.Lerp(controller.center.y, 1f, Time.deltaTime * 0.5f), controller.center.z);
+            hitbox.height = 1.8f;
+            hitbox.center = new Vector3(hitbox.center.x, Mathf.Lerp(hitbox.center.y, 1f, Time.deltaTime * 0.5f), hitbox.center.z);
+        }
+    }
+    private void SlopeCheck()
+    {
+        if (gradient < 0.9f)
+        {
+            onSteepTime += Time.deltaTime;
+        }
+        else
+        {
+            onSteepTime = 0f;
+        }
+        if (onSteepTime > 0.1f)
+        {
+            if (currentSlope > 0f)
+            {                
+                animator.SetFloat("Gradient", Mathf.Lerp(animator.GetFloat("Gradient"), 1f, Time.deltaTime));
+            }
+            else
+            {               
+                animator.SetFloat("Gradient", Mathf.Lerp(animator.GetFloat("Gradient"), -1f, Time.deltaTime));
+            }
+            if (gradient < 0.8f)
+            {
+                moveSpeed = Mathf.Lerp(moveSpeed, 0.25f * moveSpeed, 0.05f);
+            }
+            else if (gradient < 0.85f)
+            {
+                moveSpeed = Mathf.Lerp(moveSpeed, 0.5f * moveSpeed, 0.05f);
+            }
+            else
+            {
+                moveSpeed = Mathf.Lerp(moveSpeed, 0.75f * moveSpeed, 0.05f);
+            }
+            return;
+        }
+        animator.SetFloat("Gradient", Mathf.Lerp(animator.GetFloat("Gradient"), 0f, Time.deltaTime * 3f));
     }
     IEnumerator MovetoPositonRoutine(Vector3 positon)
     {
@@ -144,7 +208,7 @@ public class PlayerMovement : MonoBehaviour
             ySpeed += Physics.gravity.y * Time.deltaTime;
             if (!floating && ySpeed < 0)
             {
-                ySpeed = 0;
+                ySpeed = -1f;
             }
             controller.Move(Vector3.up * ySpeed * Time.deltaTime * 2f);
         }
@@ -255,16 +319,14 @@ public class PlayerMovement : MonoBehaviour
     }
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        currentSlope = Vector3.Dot(Vector3.up, hit.normal);
-        floating = false;
-        floatingTime = 0f;
-        if (hit.normal.x > 0f)
+        int hitLayerMask = (1 << hit.gameObject.layer);
+        if ((hitLayerMask & groundLayerMask) > 0)
         {
-            slopeSpeed = 0.25f;
-        }
-        else if (hit.normal.x < 0f)
-        {
-            slopeSpeed = 0.5f;
-        }
+            gradient = Vector3.Dot(Vector3.up, hit.normal);
+            floating = false;
+            floatingTime = 0f;
+            currentSlope = Vector3.Dot(-transform.forward, hit.normal);
+            decelerate = (hitLayerMask & slowAreaLayerMask) > 0;
+        }       
     }
 }
